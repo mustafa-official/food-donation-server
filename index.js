7eff766244cb8d0468b55485c35b036a3f9e8c33
webdev-mostofa <mdmostofaa24@gmail.com>
2024-05-11 20:11:04 +0600

    jwt token api created done and clear cookie api done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 const cors = require('cors');
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -17,6 +19,26 @@ const corsOptions = {
 //middleware
 app.use(cors(corsOptions))
 app.use(express.json());
+app.use(cookieParser());
+
+//jwt verify token middleware
+const verifyToken = (req, res, next) => {
+    const token = req.cookies?.token;
+    // console.log(token);
+    if (!token) return res.status(401).send({ message: 'unauthorized access' })
+    if (token) {
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+            if (err) {
+                console.log(err);
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
+            console.log(decoded);
+            req.user = decoded;
+            next();
+        })
+    }
+
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.elzgrcu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -35,6 +57,38 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
         const foodsCollection = client.db('foodDB').collection('foods');
+
+
+        //jwt generate
+        app.post("/jwt", async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: "365d"
+            })
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            })
+                .send({ success: true })
+        })
+
+        //clear cookie on logout user
+        app.get('/logout', (req, res) => {
+            res.clearCookie('token', {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+                maxAge: 0,
+            })
+                .send({ success: true })
+        })
+
+
+
+
+
+
         //get foods in for featured section
         app.get('/featured-foods', async (req, res) => {
             const highestQuantityFoods = await foodsCollection.aggregate([
